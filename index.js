42a49029ce4a24429eccdf298c0b4e8bcb937073
Richard Byström <richardmurakawa@gmail.com>
2022-12-06 17:52:01 +0100

    Adding integration to local MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { MongoClient } from "mongodb";
 import got from "got";
 
 const app = express();
+const dbClient = new MongoClient("mongodb://127.0.0.1:27017")
 
 app.get('/products', async (req, res) => {
     const products = await getProductList()
@@ -13,6 +15,14 @@ app.get('/products/:productId', async (req, res) => {
     const product = await getProduct(productId)
     res.send(product)
   })
+
+  app.get('/db/:productId', async (req, res) => {
+    console.log(req.params)
+    const productId = req.params.productId;
+    console.log(productId)
+    const product = await getProductFromDB(productId)
+    res.send(product)
+  })
 app.listen(3000)
 
 async function getProduct(productId) {
@@ -20,6 +30,15 @@ async function getProduct(productId) {
     .then(data => {
         return data.body
     });
+    return products;
+}
+
+async function getProductFromDB(productId) {
+    const database = dbClient.db("new");
+    const movies = database.collection("coll");
+    const query = { id: parseInt(productId) };
+    
+    const products = await movies.findOne(query);
     return products;
 }
 
