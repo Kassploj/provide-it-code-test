26d4e1db9776de8cbf6066e5231b0e969375116b
Richard Byström <richardmurakawa@gmail.com>
2022-12-06 18:39:02 +0100

    Adding writing of new products to database

--- index.js
+++ index.js
@@ -8,44 +8,61 @@ const dbClient = new MongoClient("mongodb://127.0.0.1:27017")
 app.get('/products', async (req, res) => {
     const products = await getProductList()
     res.send(products)
-  })
+})
 
 app.get('/products/:productId', async (req, res) => {
     const productId = req.params.productId;
-    const product = await getProduct(productId)
-    res.send(product)
-  })
+    let product = await getProductFromDB(productId);
+    console.log(product)
+    if (!product) {
+        product = await getProduct(productId)
+        insertToDB(product)
+    }
+    res.send(product)   
+})
 
-  app.get('/db/:productId', async (req, res) => {
-    console.log(req.params)
+app.get('/db/:productId', async (req, res) => {
     const productId = req.params.productId;
-    console.log(productId)
     const product = await getProductFromDB(productId)
     res.send(product)
-  })
+})
 app.listen(3000)
 
 async function getProduct(productId) {
     const products = await got.get(`https://fakestoreapi.com/products/${productId}`)
-    .then(data => {
-        return data.body
-    });
+        .then(data => {
+            return data.body
+        });
     return products;
 }
 
 async function getProductFromDB(productId) {
-    const database = dbClient.db("new");
-    const movies = database.collection("coll");
+    const collection =  getMongoDBCollection();
     const query = { id: parseInt(productId) };
-    
-    const products = await movies.findOne(query);
+
+    const product = await collection.findOne(query)
+        .then(data => {
+            return data;
+        });
+    return product
+}
+async function getProductList() {
+    const products = await got.get("https://fakestoreapi.com/products")
+        .then(data => {
+            return data.body
+        });
     return products;
 }
 
-async function getProductList() {
-    const products = await got.get("https://fakestoreapi.com/products")
-    .then(data => {
-        return data.body
-    });
-    return products;
+async function insertToDB(item) {
+    const collection = getMongoDBCollection()
+
+    await collection.insertOne(JSON.parse(item));
+    console.log(`Inserted ${item} into database.`)
+}
+
+function getMongoDBCollection() {
+    const collection = dbClient.db("new").collection("coll");
+
+    return collection;
 }
