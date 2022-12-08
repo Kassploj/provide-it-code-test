e23a3fd527a8367e8cc167a3a203c658bd710284
Richard Bystrom <richard.bystromext@delaval.com>
2022-12-08 09:42:20 +0100

    Adding some basic error handling

--- router.js
+++ router.js
@@ -7,34 +7,46 @@ export const storeRouter = express.Router()
 const pageSize = 8
 
 storeRouter.get('/products', async (req, res) => {
-    const { page = 0, min, max, category } = req.query;
-    const products = new ProductList(await api.getProductList(category));
-    const startIndex = page ? page * pageSize : 0;
-    const stopIndex = startIndex + pageSize;
-    products.filter(min, max)
-    products.paginate(startIndex, stopIndex)
-
-    res.send(products.getListing())
+    try {
+        const { page = 0, min, max, category } = req.query;
+        const products = new ProductList(await api.getProductList(category));
+        const startIndex = page ? page * pageSize : 0;
+        const stopIndex = startIndex + pageSize;
+        products.filter(min, max)
+        products.paginate(startIndex, stopIndex)
+    
+        res.send(products.getListing())
+    } catch (error) {
+        console.error(error)
+    }
 })
 
 storeRouter.get('/products/detailed', async (req, res) => {
-    const { page = 0, min, max, category } = req.query;
-    const products = new ProductList(await api.getProductList());
-    const startIndex = page ? page * pageSize : 0;
-    const stopIndex = startIndex + pageSize;
-    products.filter(min, max, category)
-    products.paginate(startIndex, stopIndex)
-
-    res.send(products.getListingWithDescription())
+    try {
+        const { page = 0, min, max, category } = req.query;
+        const products = new ProductList(await api.getProductList());
+        const startIndex = page ? page * pageSize : 0;
+        const stopIndex = startIndex + pageSize;
+        products.filter(min, max, category)
+        products.paginate(startIndex, stopIndex)
+    
+        res.send(products.getListingWithDescription())
+    } catch (error) {
+        console.error(error)
+    }
 })
 
 storeRouter.get('/products/:productId', async (req, res) => {
-    const productId = req.params.productId;
-    let product = await db.getProductFromDB(productId);
-    if (!product) {
-        product = await api.getProduct(productId)
-        db.insertToDB(product)
+    try {
+        const productId = req.params.productId;
+        let product = await db.getProductFromDB(productId);
+        if (!product) {
+            product = await api.getProduct(productId)
+            db.insertToDB(product)
+        }
+    
+        res.send(product)
+    } catch (error) {
+        console.error(error)
     }
-
-    res.send(product)
 })
